88b2d85e94767eb2601e42136dbd6cc2aea18e1b
gitfilix <runlevel07@gmail.com>
2019-04-26 18:04:05 +0200

    next with classbased components and getInitialProps livecycle and super-App as props

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -4,21 +4,28 @@ import Router from 'next/router';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
     <div>
-        <h1>Authentication page</h1>
+        <h1>Authentication page with {props.appName}</h1>
         <div>Login:</div>
         <input type="text"/>
         <div>password: </div>
         <input type="password" />
         <br />
         <User name="Fiiliks" age={28} />
-        
+
         <p>go back with next-Router</p>
         <button onClick={ ()=> Router.push('/')}>Go back to startpage</button>
-
     </div>
 );
 
+authIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: "Super App for auth" });
+        }, 500);
+    });
+    return promise;
+}
 
 export default authIndexPage;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 
-const indexPage = () => (
-    <div>
-        <h1>Main index page</h1>
-        <p>Lorem Ipsum... </p>
-        <p>Go to <Link href='/auth'><a>Auth</a></Link></p>
-    </div>
-);
+// class based component
+class indexPage extends Component {
+    // special (async) Lifecycle hook: get initial props
+    // static appName Component: define with getInitialProps
+    static getInitialProps(context) {
+        // logs out in terminal:
+        console.log("context of getInitialProps:",context);
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({ appName: "Super App Property with promise" });
+            }, 1000);
+        });
+    return promise;
+}
 
+
+    //retrun jsx
+    render () {
+        return (
+            <div>
+                <h1>Main index page of {this.props.appName}</h1>
+                <p>Lorem Ipsum... </p>
+                <p>Go to <Link href='/auth'><a>Auth</a></Link></p>
+            </div>
+        );
+    }
+}
 
 export default indexPage;
