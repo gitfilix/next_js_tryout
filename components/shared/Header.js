f89d88afdee954223ab615bc99645433b4018e96
gitfilix <runlevel07@gmail.com>
2019-06-01 17:19:56 +0200

    styled with scss package

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
 
+// import scss
+import '../../styles/main.scss';
+
+
 class Header extends React.Component {
     render () {
         const title = this.props.title;
         return(
             <React.Fragment>
-            <p>{title}</p>
-                <Link href="/" >
-                    <a>Home</a>
-                </Link>
-                <Link href="/about" >
-                    <a>about</a>
-                </Link>
-                <Link href="/portfolios" >
-                    <a>Portfolios</a>
-                </Link>
-                <Link href="/blogs" >
-                    <a>Blogs</a>
-                </Link>
-                <Link href="/cv" >
-                    <a>CV</a>
-                </Link>
+            <div className="headercustom">
+                <p>{title}</p>
+                    <Link href="/" >
+                        <a>Home</a>
+                    </Link>
+                    <Link href="/about" >
+                        <a>about</a>
+                    </Link>
+                    <Link href="/portfolios" >
+                        <a>Portfolios</a>
+                    </Link>
+                    <Link href="/blogs" >
+                        <a>Blogs</a>
+                    </Link>
+                    <Link href="/cv" >
+                        <a>CV</a>
+                    </Link>
+                </div>
             </React.Fragment>
         )
     }
